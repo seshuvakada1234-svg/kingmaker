9118707682c629e64330781348c1039b19e96619
Seshu Vakada <seshuvakada522@gmail.com>
2026-02-07 03:10:28 +0000

    // Example for a Pawn component // Make sure to accept a 'color' prop (c

--- src/components/icons/ChessPieces.tsx
+++ src/components/icons/ChessPieces.tsx
@@ -4,10 +4,10 @@ const sharedProps: SVGProps<SVGSVGElement> = {
   width: "100%",
   height: "100%",
   viewBox: "0 0 45 45",
+  xmlns: "http://www.w3.org/2000/svg",
 };
 
 // --- DEFS for styles ---
-// Defined inside each component to ensure they are self-contained.
 const SvgDefs = () => (
   <defs>
     <filter id="white-piece-shadow" x="-0.2" y="-0.2" width="1.4" height="1.4">
@@ -16,27 +16,25 @@ const SvgDefs = () => (
   </defs>
 );
 
-// --- PIECE PATHS (Bold, Solid, Flat Staunton Style) ---
-const KingPath = () => <path d="M22.5 6 L20.5 6 L20.5 8 L18.5 8 L18.5 10 L20.5 10 L20.5 12 L24.5 12 L24.5 10 L26.5 10 L26.5 8 L24.5 8 L24.5 6 Z M22.5,14 C17,14 14,18 14,22 L14,28 C14,29 15,30 16,30 L29,30 C30,30 31,29 31,28 L31,22 C31,18 28,14 22.5,14 Z M13.5,32 L31.5,32 L31.5,39 L13.5,39 Z" />;
-const QueenPath = () => <path d="M12.5 8 A 2 2 0 0 1 14.5 10 A 2 2 0 0 1 12.5 12 A 2 2 0 0 1 10.5 10 A 2 2 0 0 1 12.5 8 Z M22.5 8 A 2 2 0 0 1 24.5 10 A 2 2 0 0 1 22.5 12 A 2 2 0 0 1 20.5 10 A 2 2 0 0 1 22.5 8 Z M32.5 8 A 2 2 0 0 1 34.5 10 A 2 2 0 0 1 32.5 12 A 2 2 0 0 1 30.5 10 A 2 2 0 0 1 32.5 8 Z M11.5,14 L33.5,14 C30.5,20 14.5,20 11.5,14 Z M14,22 C14,27 17.5,30 22.5,30 C27.5,30 31,27 31,22 L14,22 Z M13.5,32 L31.5,32 L31.5,39 L13.5,39 Z" />;
-const RookPath = () => <path d="M14 9 L14 15 L19 15 L19 12 L26 12 L26 15 L31 15 L31 9 Z M13,17 L32,17 L32,30 L13,30 L13,17 Z M13,32 L32,32 L32,39 L13,39 Z" />;
-const BishopPath = () => <path d="M22.5 8 A 2.5 2.5 0 0 1 25 10.5 A 2.5 2.5 0 0 1 22.5 13 A 2.5 2.5 0 0 1 20 10.5 A 2.5 2.5 0 0 1 22.5 8 Z M18,15 L27,15 C29,20 29,25 22.5,29 C16,25 16,20 18,15 Z M13,31 L32,31 L32,38 L13,38 Z" />;
-const KnightPath = () => <path d="M16 10 C18 6 23 6 25 10 C27 14 24 18 20 18 C18 22 20 28 26 29 L28 29 C31 29 32 27 32 25 C32 20 28 18 28 18 C31 16 32 12 29 10 C27 8 23 8 21 11 C20 9 18 8 16 10 Z M13,31 L32,31 L32,38 L13,38 Z" />;
-const PawnPath = () => <path d="M22.5,11.5 A 3.5 3.5 0 0 1 26 15 A 3.5 3.5 0 0 1 22.5 18.5 A 3.5 3.5 0 0 1 19 15 A 3.5 3.5 0 0 1 22.5 11.5 Z M18,20 L27,20 L27,28 L18,28 Z M14,30 L31,30 L31,37 L14,37 Z" />;
+// --- PIECE PATHS (accepting props) ---
+const KingPath = (props: SVGProps<SVGPathElement>) => <path {...props} d="M22.5 6 L20.5 6 L20.5 8 L18.5 8 L18.5 10 L20.5 10 L20.5 12 L24.5 12 L24.5 10 L26.5 10 L26.5 8 L24.5 8 L24.5 6 Z M22.5,14 C17,14 14,18 14,22 L14,28 C14,29 15,30 16,30 L29,30 C30,30 31,29 31,28 L31,22 C31,18 28,14 22.5,14 Z M13.5,32 L31.5,32 L31.5,39 L13.5,39 Z" />;
+const QueenPath = (props: SVGProps<SVGPathElement>) => <path {...props} d="M12.5 8 A 2 2 0 0 1 14.5 10 A 2 2 0 0 1 12.5 12 A 2 2 0 0 1 10.5 10 A 2 2 0 0 1 12.5 8 Z M22.5 8 A 2 2 0 0 1 24.5 10 A 2 2 0 0 1 22.5 12 A 2 2 0 0 1 20.5 10 A 2 2 0 0 1 22.5 8 Z M32.5 8 A 2 2 0 0 1 34.5 10 A 2 2 0 0 1 32.5 12 A 2 2 0 0 1 30.5 10 A 2 2 0 0 1 32.5 8 Z M11.5,14 L33.5,14 C30.5,20 14.5,20 11.5,14 Z M14,22 C14,27 17.5,30 22.5,30 C27.5,30 31,27 31,22 L14,22 Z M13.5,32 L31.5,32 L31.5,39 L13.5,39 Z" />;
+const RookPath = (props: SVGProps<SVGPathElement>) => <path {...props} d="M14 9 L14 15 L19 15 L19 12 L26 12 L26 15 L31 15 L31 9 Z M13,17 L32,17 L32,30 L13,30 L13,17 Z M13,32 L32,32 L32,39 L13,39 Z" />;
+const BishopPath = (props: SVGProps<SVGPathElement>) => <path {...props} d="M22.5 8 A 2.5 2.5 0 0 1 25 10.5 A 2.5 2.5 0 0 1 22.5 13 A 2.5 2.5 0 0 1 20 10.5 A 2.5 2.5 0 0 1 22.5 8 Z M18,15 L27,15 C29,20 29,25 22.5,29 C16,25 16,20 18,15 Z M13,31 L32,31 L32,38 L13,38 Z" />;
+const KnightPath = (props: SVGProps<SVGPathElement>) => <path {...props} d="M16 10 C18 6 23 6 25 10 C27 14 24 18 20 18 C18 22 20 28 26 29 L28 29 C31 29 32 27 32 25 C32 20 28 18 28 18 C31 16 32 12 29 10 C27 8 23 8 21 11 C20 9 18 8 16 10 Z M13,31 L32,31 L32,38 L13,38 Z" />;
+const PawnPath = (props: SVGProps<SVGPathElement>) => <path {...props} d="M22.5,11.5 A 3.5 3.5 0 0 1 26 15 A 3.5 3.5 0 0 1 22.5 18.5 A 3.5 3.5 0 0 1 19 15 A 3.5 3.5 0 0 1 22.5 11.5 Z M18,20 L27,20 L27,28 L18,28 Z M14,30 L31,30 L31,37 L14,37 Z" />;
 
-// --- COMPONENTS ---
+const pieceStyleProps = {
+  strokeWidth: "1.5",
+  strokeLinejoin: "round" as const,
+};
+
+// --- WHITE PIECES ---
 export const WhitePawn = (props: SVGProps<SVGSVGElement>) => (
   <svg {...sharedProps} {...props}>
     <SvgDefs />
     <g filter="url(#white-piece-shadow)">
-      <PawnPath fill="#FFFFFF" />
-    </g>
-  </svg>
-);
-export const BlackPawn = (props: SVGProps<SVGSVGElement>) => (
-  <svg {...sharedProps} {...props}>
-    <g>
-      <PawnPath fill="#000000" />
+      <PawnPath fill="#FFFFFF" stroke="#000000" {...pieceStyleProps} />
     </g>
   </svg>
 );
@@ -44,14 +42,7 @@ export const WhiteRook = (props: SVGProps<SVGSVGElement>) => (
     <svg {...sharedProps} {...props}>
         <SvgDefs />
         <g filter="url(#white-piece-shadow)">
-            <RookPath fill="#FFFFFF" />
-        </g>
-    </svg>
-);
-export const BlackRook = (props: SVGProps<SVGSVGElement>) => (
-    <svg {...sharedProps} {...props}>
-        <g>
-            <RookPath fill="#000000" />
+            <RookPath fill="#FFFFFF" stroke="#000000" {...pieceStyleProps} />
         </g>
     </svg>
 );
@@ -59,14 +50,7 @@ export const WhiteKnight = (props: SVGProps<SVGSVGElement>) => (
     <svg {...sharedProps} {...props}>
         <SvgDefs />
         <g filter="url(#white-piece-shadow)">
-            <KnightPath fill="#FFFFFF" />
-        </g>
-    </svg>
-);
-export const BlackKnight = (props: SVGProps<SVGSVGElement>) => (
-    <svg {...sharedProps} {...props}>
-        <g>
-            <KnightPath fill="#000000" />
+            <KnightPath fill="#FFFFFF" stroke="#000000" {...pieceStyleProps} />
         </g>
     </svg>
 );
@@ -74,14 +58,7 @@ export const WhiteBishop = (props: SVGProps<SVGSVGElement>) => (
     <svg {...sharedProps} {...props}>
         <SvgDefs />
         <g filter="url(#white-piece-shadow)">
-            <BishopPath fill="#FFFFFF" />
-        </g>
-    </svg>
-);
-export const BlackBishop = (props: SVGProps<SVGSVGElement>) => (
-    <svg {...sharedProps} {...props}>
-        <g>
-            <BishopPath fill="#000000" />
+            <BishopPath fill="#FFFFFF" stroke="#000000" {...pieceStyleProps} />
         </g>
     </svg>
 );
@@ -89,14 +66,7 @@ export const WhiteQueen = (props: SVGProps<SVGSVGElement>) => (
     <svg {...sharedProps} {...props}>
         <SvgDefs />
         <g filter="url(#white-piece-shadow)">
-            <QueenPath fill="#FFFFFF" />
-        </g>
-    </svg>
-);
-export const BlackQueen = (props: SVGProps<SVGSVGElement>) => (
-    <svg {...sharedProps} {...props}>
-        <g>
-            <QueenPath fill="#000000" />
+            <QueenPath fill="#FFFFFF" stroke="#000000" {...pieceStyleProps} />
         </g>
     </svg>
 );
@@ -104,14 +74,51 @@ export const WhiteKing = (props: SVGProps<SVGSVGElement>) => (
     <svg {...sharedProps} {...props}>
         <SvgDefs />
         <g filter="url(#white-piece-shadow)">
-            <KingPath fill="#FFFFFF" />
+            <KingPath fill="#FFFFFF" stroke="#000000" {...pieceStyleProps} />
+        </g>
+    </svg>
+);
+
+// --- BLACK PIECES ---
+export const BlackPawn = (props: SVGProps<SVGSVGElement>) => (
+  <svg {...sharedProps} {...props}>
+    <g>
+      <PawnPath fill="#000000" stroke="none" />
+    </g>
+  </svg>
+);
+export const BlackRook = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...sharedProps} {...props}>
+        <g>
+            <RookPath fill="#000000" stroke="none" />
+        </g>
+    </svg>
+);
+export const BlackKnight = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...sharedProps} {...props}>
+        <g>
+            <KnightPath fill="#000000" stroke="none" />
+        </g>
+    </svg>
+);
+export const BlackBishop = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...sharedProps} {...props}>
+        <g>
+            <BishopPath fill="#000000" stroke="none" />
+        </g>
+    </svg>
+);
+export const BlackQueen = (props: SVGProps<SVGSVGElement>) => (
+    <svg {...sharedProps} {...props}>
+        <g>
+            <QueenPath fill="#000000" stroke="none" />
         </g>
     </svg>
 );
 export const BlackKing = (props: SVGProps<SVGSVGElement>) => (
     <svg {...sharedProps} {...props}>
         <g>
-            <KingPath fill="#000000" />
+            <KingPath fill="#000000" stroke="none" />
         </g>
     </svg>
 );
